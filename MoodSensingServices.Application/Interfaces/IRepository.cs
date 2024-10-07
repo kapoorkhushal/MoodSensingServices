dc992d4e129868d97057ed0f36befb206a0bbb9a
Khushal Kapoor <khushal.kapoor@nagarro.com>
2024-10-08 04:50:57 +0530

    added repository

---
+++ MoodSensingServices.Application/Interfaces/IRepository.cs
@@ -0,0 +1,73 @@
+﻿using System.Linq.Expressions;
+
+namespace MoodSensingServices.Application.Interfaces
+{
+    public interface IRepository<T> : IDisposable where T : class
+    {
+        /// <summary>
+        /// get entity by id
+        /// </summary>
+        /// <param name="Id">Guid</param>
+        /// <returns></returns>
+        T? GetById(Guid Id);
+
+        /// <summary>
+        /// Get entity data as per condition
+        /// </summary>
+        /// <param name="expression">condition to be matched</param>
+        /// <returns>returns null or single entity which satisfy the condition</returns>
+        Task<T?> Get(Expression<Func<T, bool>> expression);
+
+        /// <summary>
+        /// Get list of Entries as per condition
+        /// </summary>
+        /// <param name="expression"></param>
+        /// <returns></returns>
+        Task<List<T>> GetAll(Expression<Func<T, bool>> expression);
+
+        /// <summary>
+        /// Get all the list of entities
+        /// </summary>
+        /// <returns></returns>
+        Task<List<T>> SelectAll();
+
+        /// <summary>
+        /// save entity into database
+        /// </summary>
+        /// <returns></returns>
+        Task SaveAsync();
+
+        /// <summary>
+        /// Insert an entity
+        /// </summary>
+        /// <param name="entity"><see cref="T"/></param>
+        /// <returns></returns>
+        Task Insert(T entity);
+
+        /// <summary>
+        /// add entity into db context
+        /// </summary>
+        /// <param name="entity"></param>
+        /// <returns></returns>
+        Task Add(T entity);
+
+        /// <summary>
+        /// update an entity
+        /// </summary>
+        /// <param name="entity">enetity object</param>
+        /// <returns></returns>
+        Task Update(T entity);
+
+        /// <summary>
+        /// deletes the specified entity
+        /// </summary>
+        /// <param name="entity"><see cref="T"/></param>
+        /// <returns></returns>
+        Task Delete(T entity);
+
+        /// <summary>
+        /// Dispose the resource
+        /// </summary>
+        void Dispose();
+    }
+}
---
+++ MoodSensingServices.Infrastructure/Repository.cs
@@ -0,0 +1,130 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using MoodSensingServices.Application.Interfaces;
+using MoodSensingServices.Infrastructure.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using System.Transactions;
+
+namespace MoodSensingServices.Infrastructure
+{
+    public class Repository<T> : IRepository<T> where T : class
+    {
+        private readonly MSAContext _context;
+        private bool disposed = false;
+        private readonly string CreationDate = "CreationDate";
+        private readonly string ModifiedDate = "ModifiedDate";
+
+        public Repository(IServiceProvider serviceProvider)
+        {
+            _context = serviceProvider.CreateScope().ServiceProvider.GetRequiredService<MSAContext>();
+        }
+
+        /// <inheritdoc />
+        public virtual T? GetById(Guid id)
+        {
+            using (new TransactionScope(TransactionScopeOption.Required, new TransactionOptions { IsolationLevel = IsolationLevel.ReadUncommitted }))
+            {
+                return _context.Set<T>().Find(id);
+            }
+        }
+
+        /// <inheritdoc />
+        public virtual async Task<T?> Get(Expression<Func<T, bool>> expression)
+        {
+            return await _context.Set<T>()
+                .Where(expression)
+                .FirstOrDefaultAsync().ConfigureAwait(false);
+        }
+
+        /// <inheritdoc />
+        public virtual async Task<List<T>> GetAll(Expression<Func<T, bool>> expression)
+        {
+            return await _context.Set<T>()
+                .Where(expression)
+                .ToListAsync().ConfigureAwait(false);
+        }
+
+        /// <inheritdoc />
+        public virtual async Task<List<T>> SelectAll()
+        {
+            return await _context.Set<T>()
+                .ToListAsync().ConfigureAwait(false);
+        }
+
+        /// <inheritdoc />
+        public virtual async Task SaveAsync()
+        {
+            var addedEntities = _context.ChangeTracker.Entries().Where(entry => entry.State == EntityState.Added).ToList();
+            addedEntities.ForEach(entry =>
+            {
+                entry.Property(CreationDate).CurrentValue = DateTime.UtcNow;
+                entry.Property(ModifiedDate).CurrentValue = DateTime.UtcNow;
+            });
+
+            await _context.SaveChangesAsync().ConfigureAwait(false);
+        }
+
+        /// <inheritdoc />
+        public virtual async Task Insert(T entity)
+        {
+            _context.Set<T>().Add(entity);
+            var addedEntities = _context.ChangeTracker.Entries().Where(entry => entry.State == EntityState.Added).ToList();
+            addedEntities.ForEach(entry =>
+            {
+                entry.Property(CreationDate).CurrentValue = DateTime.UtcNow;
+                entry.Property(ModifiedDate).CurrentValue = DateTime.UtcNow;
+            });
+
+            await _context.SaveChangesAsync().ConfigureAwait(false);
+        }
+
+        /// <inheritdoc />
+        public virtual async Task Add(T entity)
+        {
+            _context.Set<T>().Add(entity);
+        }
+
+        /// <inheritdoc />
+        public virtual async Task Update(T entity)
+        {
+            _context.Entry(entity).State = EntityState.Modified;
+            await _context.SaveChangesAsync().ConfigureAwait(false);
+        }
+
+        /// <inheritdoc />
+        public void Dispose()
+        {
+            this.Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        /// <summary>
+        /// Dispose the context object
+        /// </summary>
+        /// <param name="disposing">Disposing flag</param>
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!this.disposed)
+            {
+                if (disposing)
+                {
+                    _context.Dispose();
+                }
+
+                this.disposed = true;
+            }
+        }
+
+        /// <inheritdoc />
+        public virtual async Task Delete(T entity)
+        {
+            _context.Set<T>().Remove(entity);
+            await _context.SaveChangesAsync().ConfigureAwait(false);
+        }
+    }
+}
