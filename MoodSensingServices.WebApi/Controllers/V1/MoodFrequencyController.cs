da88e612364e004250088f1dfe120062c981801c
Khushal Kapoor <khushal.kapoor@nagarro.com>
2024-10-08 17:34:16 +0530

    added controllers

---
+++ MoodSensingServices.WebApi/Controllers/V1/MoodFrequencyController.cs
@@ -0,0 +1,8 @@
+﻿using MoodSensingServices.Webapi.Controllers;
+
+namespace MoodSensingServices.WebApi.Controllers.V1
+{
+    public class MoodFrequencyController: BaseApiController
+    {
+    }
+}
